49dd4fbe3a106f2136bee1a18042e404b46b3d5c
ram-chadar <javaprojectwork2715@gmail.com>
2025-06-27 18:03:51 +0530

    initial

---
+++ src/com/Operation.java
@@ -0,0 +1,19 @@
+package com;
+
+public class Operation {
+
+	public static int add(int a, int b) {
+		return 0;
+
+	}
+
+	public static int subtract(int a, int b) {
+		return 0;
+	}
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
