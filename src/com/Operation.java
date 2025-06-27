370f5a3660162e4df122e3bdd00f5134ee877233
ram-chadar <javaprojectwork2715@gmail.com>
2025-06-27 18:21:11 +0530

    addition

--- src/com/Operation.java
+++ src/com/Operation.java
@@ -3,11 +3,13 @@ package com;
 public class Operation {
 
 	public static int add(int a, int b) {
-		return 0;
+		int c=a+b;
+		return c;
 
 	}
 
 	public static int subtract(int a, int b) {
+		int c=a-b;
 		return 0;
 	}
 
